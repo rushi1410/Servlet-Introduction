11dd98005d4c21b0584517c564f5e3d403b605a2
rushi1410 <mr.rushikesh.molkire@outlook.com>
2022-08-23 21:32:47 +0530

    Added:Welcome To Servlet Introduction Program

---
+++ src/main/java/com/myfirstservlet/SimpleServlet.java
@@ -0,0 +1,9 @@
+package com.myfirstservlet;
+public class SimpleServlet  {
+    public static void main(String[] args) {
+        System.out.println("Welcome To Servlet Program");
+    }
+}
+
+
+
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>Hello World Welcome To First Servlet Program!</h2>
+</body>
+</html>
