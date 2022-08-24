6f006489757bc5a78cbda757b9989bd0a9d93782
rushi1410 <mr.rushikesh.molkire@outlook.com>
2022-08-24 23:48:20 +0530

    Refatored:Extend The Servlet To Accept A Valid Name

--- src/main/java/com/myfirstservlet/LoginServlet.java
+++ src/main/java/com/myfirstservlet/LoginServlet.java
@@ -8,32 +8,35 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
-
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 @WebServlet(
         description = "Login Servlet testing",
         urlPatterns = { "/LoginServlet" },
         initParams = { @WebInitParam(name="user",value="Rushikesh"),
-                       @WebInitParam(name="password",value="rushikeshm")
+                       @WebInitParam(name="password",value="Rushikeshm")
         }
 )
 public class LoginServlet extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse response) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        String user = req.getParameter("user");
-        String pwd = req.getParameter("pwd");
+        String user = request.getParameter("user");
+        boolean validuser = (user != null) && user.matches("[A-Z]{1}[a-z]{3,6}");
+
+        String pwd = request.getParameter("pwd");
 
         String userID = getServletConfig().getInitParameter("user");
         String password = getServletConfig().getInitParameter("password");
 
         if (userID.equals(user) && password.equals(pwd)){
-            req.setAttribute("user",user);
-            req.getRequestDispatcher("LoginSuccess.jsp").forward(req,response);
+            request.setAttribute("user",user);
+            request.getRequestDispatcher("LoginSuccess.jsp").forward(request,response);
         } else {
             RequestDispatcher rd = getServletContext().getRequestDispatcher("/login.html");
             PrintWriter out = response.getWriter();
             out.println("<font color=red>Either user name or password is wrong.</font>");
-            rd.include(req,response);
+            rd.include(request,response);
         }
     }
 }
